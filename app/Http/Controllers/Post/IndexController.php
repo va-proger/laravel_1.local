73d655c6dc880e9e40ddc6008edc0d9597187e41
vproger <vproger@vk.com>
2022-12-09 02:04:06 +0400

    api TypeError: array_merge(): Argument #2 must be of type array, int given in file /var/www/html/laravel_1.local/vendor/tymon/jwt-auth/src/JWT.php on line 240

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\PostFilter;
 use App\Http\Requests\Post\FilterRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 
 class IndexController extends BaseController
@@ -12,11 +13,14 @@ class IndexController extends BaseController
     public function __invoke(FilterRequest $request)
     {
 
-//        $this->authorize('view', auth()->user());
         $data = $request->validated();
-        $filter = app()->make(PostFilter::class, ['queryParams'=> array_filter($data)]);
-        $posts = Post::filter($filter)->paginate(10);
 
+        $page = $data['page'] ?? 1;
+        $per_page = $data['per_page'] ?? 10;
+
+        $filter = app()->make(PostFilter::class, ['queryParams'=> array_filter($data)]);
+        $posts = Post::filter($filter)->paginate($per_page, ['*'], 'page', $page);
+//        return PostResource::collection($posts);
         return view('post.index', compact('posts'));
     }
 }
--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post\StoreRequest;
+use App\Http\Resources\Post\PostResource;
 use App\Models\Post;
 
 class StoreController extends BaseController
@@ -11,8 +12,11 @@ class StoreController extends BaseController
     public function __invoke(StoreRequest $request)
     {
         $data = $request->validated();
-        $this->service->store($data);
 
+        $post = $this->service->store($data);
+
+
+//        return new PostResource($post);
         return redirect()->route('post.index');
     }
 }
